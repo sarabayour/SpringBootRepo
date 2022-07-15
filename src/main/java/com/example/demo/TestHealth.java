89d67340ff10ac4557fad60a25ef125a25d4554c
sarabay <sarabay@ext.inditex.com>
2022-07-15 23:53:12 +0100

    custom health indicator

---
+++ src/main/java/com/example/demo/TestHealth.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestHealth implements HealthIndicator {
+
+  @Override
+  public Health health() {
+    if(isHealthOK()){
+      return Health.up().withDetail("Dummy Service" , "is running").build();
+    }
+    return Health.down().withDetail("Dummy Service" , "is down").build();
+
+  }
+
+  private boolean isHealthOK () {
+    // logic here
+    return false;
+  }
+}
